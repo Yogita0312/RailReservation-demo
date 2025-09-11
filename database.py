71eb66dc863d7ee6dcf33239d4b5592b9c2972aa
Yogita@0312 <yogita.zirange@infarsight.com>
2025-09-11 09:24:50 +0530

    Switch from pyodbc to pymssql for MSSQL connection

--- database.py
+++ database.py
@@ -36,10 +36,13 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
+#engine = create_engine(
+#    DATABASE_URL,
+#   fast_executemany=True,  
+#    pool_pre_ping=True,     
+#)
 engine = create_engine(
-    DATABASE_URL,
-    fast_executemany=True,  
-    pool_pre_ping=True,     
+    DATABASE_URL      
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
